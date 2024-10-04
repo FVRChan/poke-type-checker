96ec8edb480c12f67cf9a98cdea058c2a5f8b253
fvrchan <sawasakifvr@gmail.com>
2024-10-04 14:30:58 +0900

    temp

--- poke-js/src/pokemon.ts
+++ poke-js/src/pokemon.ts
@@ -6,7 +6,7 @@ import { filterMoveList } from "./util";
 import { abilityList } from "./util";
 import { Ability } from "./ability-list";
 import { EffortSlider } from "./EffortSlider";
-import { OFFENCE_ITEM_RATE, OFFENCE_ITEM_RATE_ID } from "./OffenceItem";
+import { OFFENCE_ITEM_RATE_ID } from "./OffenceItem";
 
 export interface Pokemon {
   id: number;
@@ -23,88 +23,35 @@ export interface Pokemon {
   ability_id_list: Array<number>;
   move_id_list: Array<number>;
   weight: number;
-  // effective_value: EffectiveValue;
-  // effective_slider_step: EffortSlider;
-  // personality: Personality;
   usage_rate: number;
   often_used_move: Array<number>;
   often_used_tokusei: Array<number>;
-  // selected_move?: Move;
-  // selected_move_id?: number;
   move_list?: Move[];
   ability_list?: Ability[];
-  // selected_hit_number?: number;
-  // selected_ability?: Ability;
-  // selected_ability_id?: number;
-  // terasu_type?: number;
-  // adapt_deffence_ability?: boolean;
 }
 
 export interface PokemonOffenceInterface {
   pokemon: Pokemon;
-  // id: number;
-  // name_ja: string;
-  // hp: number;
-  // attack: number;
-  // defense: number;
-  // special_attack: number;
-  // special_defense: number;
-  // speed: number;
-  // picture_url: string;
-  // species_id: number;
-  // type_id_list: Array<number>;
-  // ability_id_list: Array<number>;
-  // move_id_list: Array<number>;
-  // weight: number;
   effective_value: EffectiveValue;
   effective_slider_step: EffortSlider;
   personality: Personality;
-  // usage_rate: number;
-  // often_used_move: Array<number>;
-  // often_used_tokusei: Array<number>;
   selected_move?: Move;
   selected_move_id?: number;
-  // move_list?: Move[];
-  // ability_list?: Ability[];
   selected_hit_number?: number;
   selected_ability?: Ability;
   selected_ability_id?: number;
   selected_offencete_item_rate_id?: OFFENCE_ITEM_RATE_ID;
   terasu_type?: number;
-  // adapt_deffence_ability?: boolean;
 }
 
 export interface PokemonDefenceInterface {
   pokemon: Pokemon;
-  // id: number;
-  // name_ja: string;
-  // hp: number;
-  // attack: number;
-  // defense: number;
-  // special_attack: number;
-  // special_defense: number;
-  // speed: number;
-  // picture_url: string;
-  // species_id: number;
-  // type_id_list: Array<number>;
-  // ability_id_list: Array<number>;
-  // move_id_list: Array<number>;
-  // weight: number;
   effective_value: EffectiveValue;
   effective_slider_step: EffortSlider;
   personality: Personality;
-  // usage_rate: number;
-  // often_used_move: Array<number>;
-  // often_used_tokusei: Array<number>;
-  // selected_move?: Move;
-  // selected_move_id?: number;
-  // move_list?: Move[];
-  // ability_list?: Ability[];
-  // selected_hit_number?: number;
   selected_ability?: Ability;
   selected_ability_id?: number;
   terasu_type?: number;
-  // adapt_deffence_ability?: boolean;
 }
 export function toOffence(p: Pokemon): PokemonOffenceInterface {
   const ret = {
@@ -140,7 +87,6 @@ export function toDefence(p: Pokemon): PokemonDefenceInterface {
   const ret = {
     pokemon: p,
     selected_ability: p.ability_list ? p.ability_list[0] : undefined,
-    // selected_move: p.move_list ? p.move_list[0] : undefined,
     effective_slider_step: {
       hp: 0,
       attack: 32,
@@ -176,60 +122,12 @@ export const pokemon_list = all_pokemon_list
   })
   .filter((p) => p.usage_rate < 999);
 pokemon_list.forEach((p) => {
-  // p.effective_slider_step = {
-  //   hp: 0,
-  //   attack: 32,
-  //   defense: 0,
-  //   special_attack: 32,
-  //   special_defense: 0,
-  // };
-  // p.effective_value = {
-  //   hp: 0,
-  //   attack: 0,
-  //   defense: 0,
-  //   special_attack: 0,
-  //   special_defense: 0,
-  // };
-  // p.personality = {
-  //   attack: 1.0,
-  //   defense: 1.0,
-  //   special_attack: 1.0,
-  //   special_defense: 1.0,
-  // };
   p.move_list = filterMoveList(p);
   p.ability_list = abilityList(p);
-  // 2回叩くのアレなので要改善
 });
-// pokemon_list.forEach((p) => {
-//   p.selected_ability = p.ability_list ? p.ability_list[0] : undefined;
-//   p.selected_move = p.move_list ? p.move_list[0] : undefined;
-// });
-// pokemon_list.forEach((p) => {
-//   p.selected_ability_id = p.selected_ability?.id;
-//   p.selected_move_id = p.selected_move?.id;
-// });
-
-// export function pokemon_array(separate_number: number) {
-//   const t = isMobile ? 4 : 99;
-//   const local_separate_number = Math.min(t, separate_number);
-//   let ret_matrix = [] as Array<Array<Pokemon>>;
-//   let temp_list = [] as Array<Pokemon>;
-//   pokemon_list.forEach((p, _) => {
-//     temp_list.push(p);
-//     if (temp_list.length >= local_separate_number) {
-//       ret_matrix.push(temp_list);
-//       temp_list = [];
-//     }
-//   });
-//   if (temp_list.length > 0) {
-//     ret_matrix.push(temp_list);
-//     temp_list = [];
-//   }
-//   return ret_matrix;
-// }
 
 export function pokemon_array(separate_number: number) {
-  const t = isMobile ? 4 : 7;
+  const t = isMobile ? 2 : 7;
   const local_separate_number = Math.min(t, separate_number);
   let ret_matrix = [] as Array<Array<PokemonDefenceInterface>>;
   let temp_list = [] as Array<PokemonDefenceInterface>;
